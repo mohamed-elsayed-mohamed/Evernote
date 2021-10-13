99679dccea2e0171a119d253056e4b977c3b7e1c
Mohamed Elsayed <mohamed_elsayed.1995@yahoo.com>
2021-10-13 04:24:05 +0200

    Init DatabaseHelper

---
+++ Evernote/ViewModel/Helpers/DatabaseHelper.cs
@@ -0,0 +1,48 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Evernote.ViewModel.Helpers
+{
+    class DatabaseHelper
+    {
+        private static string dbFile = Path.Combine(Environment.CurrentDirectory, "notesdb.db3");
+
+        public static bool Insert<T>(T item)
+        {
+            using(SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<T>();
+                return connection.Insert(item) > 0;
+            }
+        }
+
+        public static bool Update<T>(T item)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<T>();
+                return connection.Update(item) > 0;
+            }
+        }
+
+        public static bool Delete<T>(T item)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<T>();
+                return connection.Delete(item) > 0;
+            }
+        }
+
+        public static List<T> Read<T>() where T : new()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<T>();
+                return connection.Table<T>().ToList();
+            }
+        }
+    }
+}
